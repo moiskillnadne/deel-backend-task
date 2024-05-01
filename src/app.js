7e3df7a868aa7c47eaf8aef0d950f3f8f5a345e5
Viktor Riabkov <vriabkov@scnsoft.com>
2024-05-01 14:18:39 +0400

    Feat: /jobs/:jobId/pay (Allows client to pay for the job)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Op } = require('sequelize')
+const { Op, Transaction } = require('sequelize')
 
 const { sequelize } = require('./model')
 const { getProfile } = require('./middleware/getProfile')
@@ -76,6 +76,70 @@ app.get('/jobs/unpaid', getProfile, async (req, res) => {
 
 
     res.json(job)
+})
+
+app.post('/jobs/:jobId/pay', getProfile, async (req, res) => {
+    const { Job, Contract, Profile } = req.app.get('models')
+    const { id: profileId } = req.profile
+    const { jobId } = req.params
+
+    const transaction = await sequelize.transaction({
+        // isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE -- Not supported by sqlite
+    })
+
+    try {
+        const job = await Job.findOne({
+            // Find the job by id, that is unpaid and belongs to an active contract
+            // and the client is the one making the payment
+            where: { 
+                id: jobId, 
+                paid: null 
+            },
+            include: [{ 
+                model: Contract,
+                where: { 
+                    status: 'in_progress', 
+                    clientId: profileId
+                }
+            }]
+        }, { transaction })
+
+        if(!job) {
+            throw new Error('Unpaid job by job id not found')
+        }
+
+
+        const client = await Profile.findByPk(job.Contract.ClientId, { transaction });
+        const contractor = await Profile.findByPk(job.Contract.ContractorId, { transaction });
+
+        const amount = job.price
+
+        if (client.balance < amount) {
+          throw new Error('Insufficient funds');
+        }
+    
+        client.balance -= amount; // Decrease the balance of the client
+        contractor.balance += amount; // Increase the balance of the contractor
+    
+        await client.save({ transaction });
+        await contractor.save({ transaction });
+
+
+        // Update the job to be paid and set the payment date
+        job.paid = true;
+        job.paymentDate = new Date();
+
+        await job.save({ transaction });
+    
+
+        // Commit the transaction
+        await transaction.commit();
+        res.json({ message: 'Payment processed successfully' });
+      } catch (error) {
+        console.error(error);
+        await transaction.rollback();
+        res.status(404).json({ message: 'Transaction failed' });
+      }
 })
 
 module.exports = app;
