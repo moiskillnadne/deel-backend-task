2011fd1ee724bafca74e9995ae0dda4904a83cad
Viktor Riabkov <vriabkov@scnsoft.com>
2024-05-01 12:17:46 +0400

    Fix: /contracts/:id

--- src/app.js
+++ src/app.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+// const { Op } = require('sequelize')
+
+const { sequelize } = require('./model')
+const { getProfile } = require('./middleware/getProfile')
+
 const app = express();
+
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
  * @returns contract by id
  */
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
+    const { Contract } = req.app.get('models')
+    const { id } = req.params
+    const { id: profileId } = req.profile
+
+    const contract = await Contract.getById(id, profileId)
+
+    if(!contract) return res.status(404).json({ message: 'Not found' }).end()
     res.json(contract)
 })
 module.exports = app;
