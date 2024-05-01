835fcd4cc505f2dbb7aa761a96a742bec0bc09a6
Viktor Riabkov <vriabkov@scnsoft.com>
2024-05-01 23:32:23 +0400

    Create JSDoc for getProfile middleware

--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -1,3 +1,11 @@
+/**
+ * Middleware function to get the profile.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @returns {Promise<void>} - A promise that resolves when the middleware is complete.
+ */
 export const getProfile = async (req, res, next) => {
   const { Profile } = req.app.get("models")
   const profile = await Profile.findOne({ where: { id: req.get("profile_id") || 0 } })
